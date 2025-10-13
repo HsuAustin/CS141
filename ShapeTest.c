bb1a5eb1e6e3209cb60a18e5534b730362808ec8
Chi-Min Hsu <chiminh1@uci.edu>
2025-10-13 00:15:08 -0700

    finished doing virtual methods

---
+++ ShapeTest.c
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+typedef double (*double_method_type)(void*);
+typedef void (*void_method_type)(void*);
+typedef const char* (*cstring_method_type)(void*);
+
+typedef union {
+    double_method_type double_method;
+    void_method_type void_method;
+    cstring_method_type cstring_method;
+} VirtualTableEntry;
+
+typedef VirtualTableEntry* VTableType;
+
+#define DIMS_INDEX 0
+#define AREA_INDEX 1
+#define DRAW_INDEX 2
+
+#define PI 3.14159
+
+struct Shape {
+    VTableType VPointer;
+    const char* name;
+};
