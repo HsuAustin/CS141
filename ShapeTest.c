960e6db1bbc14de389aa434f1f17165bef16bed4
Chi-Min Hsu <chiminh1@uci.edu>
2025-10-13 00:41:00 -0700

    done shape

--- ShapeTest.c
+++ ShapeTest.c
@@ -16,9 +16,9 @@ typedef union {
 
 typedef VirtualTableEntry* VTableType;
 
-#define DIMS_INDEX 0
-#define AREA_INDEX 1
-#define DRAW_INDEX 2
+#define AREA_INDEX 0
+#define DRAW_INDEX 1
+#define DIMS_INDEX 2
 
 #define PI 3.14159
 
@@ -27,7 +27,12 @@ struct Shape {
     const char* name;
 };
 
-static Shape* Shape_shape(Shape* _this, const char* nm) {
-    _this -> name = nm;
-    return _this;
-}
+static double Shape_area(Shape* _this) {return 0.0;}
+static void Shape_draw(Shape* _this) {}
+static const char* Shape_dims(Shape* _this) {return "";}
+
+static VirtualTableEntry Shape_VTable[] = {
+    {.double_method = (double_method_type) Shape_area},
+    {.void_method = (void_method_type) Shape_draw},
+    {.cstring_method = (cstring_method_type) Shape_dims}
+};
