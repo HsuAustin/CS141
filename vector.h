354cff3436d26e929ce642d9e0631e448ccddd50
Chi-Min Hsu <chiminh1@uci.edu>
2025-10-20 23:17:04 -0700

    done vector.h, working on vectest.cpp

--- vector.h
+++ vector.h
@@ -59,7 +59,52 @@ class vector {
             for (size_t i = 0; i < n; i++) {
                 T a = (i < sz) ? buf[i] : T{};
                 T b = (i < v.sz) ? v.buf[i] : T{};
-                out.
+                out.buf[i] = a + b;
             }
+            return out;
         }
-}
+
+        const Vector& operator = (const Vector &v) {
+            if (this == &v) {return *this;}
+            delete [] buf;
+            buf = nullptr;
+            sz = v.sz;
+            if (sz > 0) {
+                buf = new T[sz];
+                for (size_t i = 0; i < sz; i++) {buf[i] = v.buf[i];}
+            }
+            return *this;
+        }
+
+        bool operator == (const Vector &v) const {
+            if (sz != v.sz) {return false;}
+            for (size_t i = 0; i < sz; i++) {if (!(buf[i] == v.buf[i])) {return false;}}
+            return true;
+        }
+
+        bool operator != (const Vector &v) const {return !(*this == v);}
+
+        inline friend Vector operator * (const int scale, const Vector &v) {
+            Vector<T> out(v.sz);
+            for (size_t i = 0; i < v.sz; i++) {out.buf[i] = static_cast<T> scale * v.buf[i];}
+            return out;
+        }
+
+        inline friend Vector operator + (const int adder, const Vector &v) {
+            Vector<T> out(v.sz);
+            for (size_t i = 0; i < v.sz; i++) {out.buf[i] = v.buf[i] + static_cast<T> adder;}
+            return out;
+        }
+
+        inline friend ostream& operator << (ostream &o, const Vector &v) {
+            o << '(';
+            for (size_t i = 0; i < v.sz; i++) {
+                o << v.buf[i];
+                if (i + 1 < v.sz) {o << ", ";}
+            }
+            o << ')';
+            return o;
+        }
+};
+
+#endif
