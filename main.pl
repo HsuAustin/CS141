b96f79edb4a8b8e3d25b47ef552fd1ae7a9aafb1
Chi-Min Hsu <chiminh1@uci.edu>
2025-11-08 00:52:25 -0800

    done, ready to submit

--- main.pl
+++ main.pl
@@ -7,10 +7,9 @@ my_member(X, [_|T]) :- my_member(X, T).
 my_append([], L, L).
 my_append([H|T], L2, [H|R]) :- my_append(T, L2, R).
 
-my_reverse(L, R) :- my_rev(L, [], R).
-
-my_rev([], Acc, Acc).
-my_rev([H|T], Acc, R) :- my_rev(T, [H|Acc], R).
+my_reverse_helper([], Acc, Acc).
+my_reverse_helper([H|T], Acc, R) :- my_reverse_helper(T, [H|Acc], R).
+my_reverse(L, R) :- my_reverse_helper(L, [], R).
 
 my_nth(L, 1, L) :- !.
 my_nth([], _N, []) :- !.
@@ -27,29 +26,29 @@ my_subset(_P, [], []).
 my_subset(P, [H|T], [H|R]) :- Goal =.. [P, H], call(Goal), !, my_subset(P, T, R).
 my_subset(P, [_|T], R) :- my_subset(P, T, R).
 
-my_add(A, B, R) :- my_add_carry(A, B, 0, R).
-
-my_add_carry([], [], 0, []).
-my_add_carry([], [], C, [C]) :- C > 0.
-my_add_carry([A|TA], [], C, [S|R]) :- add_digits(A, 0, C, S, C1), my_add_carry(TA, [], C1, R).
-my_add_carry([], [B|TB], C, [S|R]) :- add_digits(0, B, C, S, C1), my_add_carry([], TB, C1, R).
-my_add_carry([A|TA], [B|TB], C, [S|R]) :- add_digits(A, B, C, S, C1), my_add_carry(TA, TB, C1, R).
-
-add_digits(A, B, C, S, Cout) :- Sum is A + B + C, S is Sum mod 10, Cout is Sum // 10.
+my_add_helper_1(A, B, C, S, Cout) :- Sum is A + B + C, S is Sum mod 10, Cout is Sum // 10.
+my_add_helper_2([], [], 0, []).
+my_add_helper_2([], [], C, [C]) :- C > 0.
+my_add_helper_2([A|TA], [], C, [S|R]) :- my_add_helper_1(A, 0, C, S, C1), my_add_helper_2(TA, [], C1, R).
+my_add_helper_2([], [B|TB], C, [S|R]) :- my_add_helper_1(0, B, C, S, C1), my_add_helper_2([], TB, C1, R).
+my_add_helper_2([A|TA], [B|TB], C, [S|R]) :- my_add_helper_1(A, B, C, S, C1), my_add_helper_2(TA, TB, C1, R).
+my_add(A, B, R) :- my_add_helper_2(A, B, 0, R).
 
 my_merge([], L, L).
 my_merge(L, [], L).
 my_merge([H1|T1], [H2|T2], [H1|R]) :- H1 =< H2, !, my_merge(T1, [H2|T2], R).
 my_merge([H1|T1], [H2|T2], [H2|R]) :- H1 > H2, my_merge([H1|T1], T2, R).
 
-my_sublist(S, L) :- my_append(_, Rest, L), my_append(S, _, Rest).
+my_sublist_helper(_, []).
+my_sublist_helper([H1|T1], [H2|T2]) :- H1 = H2, my_sublist_helper(T1, T2).
+my_sublist(S, L) :- my_sublist_helper(L, S).
+my_sublist(S, [_|T]) :- my_sublist(S, T).
 
 my_assoc(K, [K,V|_], V) :- !.
 my_assoc(K, [_K,_V|T], R) :- my_assoc(K, T, R).
 
+my_replace_helper([K,V|_], K, V) :- !.
+my_replace_helper([_|Rest], K, V) :- my_replace_helper(Rest, K, V).
 my_replace(_AL, [], []).
-my_replace(AL, [H|T], [V|R]) :- key_value(AL, H, V), !, my_replace(AL, T, R).
+my_replace(AL, [H|T], [V|R]) :- my_replace_helper(AL, H, V), !, my_replace(AL, T, R).
 my_replace(AL, [H|T], [H|R]) :- my_replace(AL, T, R).
-
-key_value([K,V|_], K, V) :- !.
-key_value([_|Rest], K, V) :- key_value(Rest, K, V).
